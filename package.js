242354317781321edea7c9ed26f2a4ff0ee1e920
Joshua Ohlman <codewriterohs@gmail.com>
2016-02-25 17:40:18 -0600

    Upgrade to Meteor 1.2.1

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
   name: 'useful:collection-views',
-  version: '0.0.1',
+  version: '0.2.0',
   summary: 'Creates a "where" method for your collections for narrowing your queries',
   git: 'https://github.com/usefulio/collection-views.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.2.1');
   api.addFiles('collection-views.js');
 
   api.use([
     'mongo'
+    , 'ecmascript'
     , 'underscore'
   ]);
 });
